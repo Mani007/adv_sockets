1c25fe9caaf97ff46324dcd55842c98ac9ac315a
Mohnish <mohnish.a007@gmail.com>
2024-08-10 00:56:48 +0100

    socket.io setup ready

--- backend/sockserver.js
+++ backend/sockserver.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const dotenv = require('dotenv')
+import express from'express';
+import dotenv from 'dotenv';
 dotenv.config()
+
+import {Server} from 'socket.io';
+import { createServer} from 'http'
+
 const app = express()
+const server = new createServer(app)
+const io = new Server(server)
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+io.on('connection', (socket) => {
+    console.log('A user connected')
+    console.log(`ID is ${socket.id}`)
 })
 
 app.listen(port, () => {
